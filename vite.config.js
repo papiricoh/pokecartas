a1fb5e1ea69f47645faff2452402ab6fe82fe8d6
Papiricoh <slendeman0200@gmail.com>
2023-03-06 16:29:18 +0100

    Updated viite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://papiricoh.github.io/pokecartas/',
+  base: '/pokecartas/',
   plugins: [vue()],
   resolve: {
     alias: {
